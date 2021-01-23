f8a624758a53b92cde9acc0ca84e1c3893a4ff15
Mattis <mattislt@sf-nett.no>
2021-01-23 18:46:51 +0100

    feat: Implemented parameter experiments

--- src/agent/table_approximator.py
+++ src/agent/table_approximator.py
@@ -2,7 +2,6 @@
 import numpy as np
 from abstract_classes.approximator import Approximator
 from environment.universal_state import UniversalState
-from utils.config_parser import Config
 
 
 class TableApproximator(Approximator):
@@ -10,12 +9,12 @@ class TableApproximator(Approximator):
     def __init__(self) -> None:
         super().__init__()
 
-    def compute_state_values(self, temporal_difference_error: float, eligibilities: dict) -> None:
+    def compute_state_values(self, temporal_difference_error: float, eligibilities: dict, learning_rate: float) -> None:
         for key, eligibility in eligibilities.items():
-            self.state_values[key] += Config.critic_learning_rate * temporal_difference_error * eligibility
+            self.state_values[key] += learning_rate * temporal_difference_error * eligibility
 
-    def compute_state_value(self, state: UniversalState, td_error: float, eligibilities: dict) -> None:
-        self.state_values[str(state)] = self.state_values[str(state)] + Config.critic_learning_rate * eligibilities[str(state)] * td_error
+    def compute_state_value(self, state: UniversalState, td_error: float, eligibilities: dict, learning_rate: float) -> None:
+        self.state_values[str(state)] = self.state_values[str(state)] + learning_rate * eligibilities[str(state)] * td_error
 
     def initialize_state_value(self, state: UniversalState) -> None:
         self.state_values.setdefault(str(state), np.random.uniform(-0.01, 0.01))
